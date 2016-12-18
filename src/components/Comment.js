4e558d23c125d7025cd6a73375521a60ce7ec553
Roman <kaboom.ua@gmail.com>
2016-12-18 18:12:29 +0200

    Update Comment.js

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,6 +4,7 @@ export default class Comment extends Component {
 
     render() {
         const comment = this.props.comment;
+    //лучше не вносить li в Comment, это значительно уменьшает возможность переиспользовать компонент
         return (
             <li>
                 <h5>{ comment.user } ID = { comment.id }</h5>
