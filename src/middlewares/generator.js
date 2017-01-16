21866d8585ee6068fb722fa0e4f4d34e6b7cae5d
Roman <kaboom.ua@gmail.com>
2017-01-16 10:52:35 +0200

    Update generator.js

--- src/middlewares/generator.js
+++ src/middlewares/generator.js
@@ -7,10 +7,12 @@ function generate() {
 }
 
 export default store => next => action => {
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
     if (action.type === ADD_COMMENT) {
         const {article, comment} = action.payload
         if (!comment.id) {
             const substitute = addComment(article, {...{id: generate()}, ...comment} )
+            //лучше используй next
             store.dispatch(substitute)
         } else {
             next(action)
